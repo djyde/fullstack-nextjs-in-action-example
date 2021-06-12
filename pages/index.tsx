f84d8006e31d7a5bc8921353060f7c896f6ecf77
Randy <randypriv@gmail.com>
2021-06-13 01:06:35 +0800

    hn homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,56 @@
 // pages/index.tsx
 import React from 'react'
+import { getUserFromReq } from '../utils.server'
 
-function IndexPage() {
+function SubmitLinkForm() {
+  return (
+    <>
+      <h2>Submit link</h2>
+      <div>
+        <label>URL: </label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>Title: </label>
+        <input type="text" />
+      </div>
+    </>
+  )
+}
+
+function IndexPage(props: {
+  user?: {
+    name: string
+  }
+}) {
 
   return (
     <>
-      hello world
+      <div>
+        {props.user ? <>
+          <span>Hi, {props.user.name}, </span>
+          <a href="/api/logout">Logout</a>
+        </> : <>
+          <a href="/login">Login</a>
+        </>}
+      </div>
+
+      {/* Only signed in user can see the submit link form */}
+      {props.user && <div>
+        <SubmitLinkForm />
+      </div>}
     </>
   )
+}
+
+export async function getServerSideProps(ctx) {
+  const user = await getUserFromReq(ctx.req)
+
+  return {
+    props: {
+      user: user ? { name: user?.name } : null
+    }
+  }
 }
 
 export default IndexPage
